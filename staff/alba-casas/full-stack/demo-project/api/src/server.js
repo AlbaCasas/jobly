880994eac994270d412d99b2fb68616175ff006b
Alba Casas <albacasasarzua1b@gmail.com>
2022-03-02 15:13:57 +0100

    Add delete user logic

--- staff/alba-casas/full-stack/demo-project/api/src/server.js
+++ staff/alba-casas/full-stack/demo-project/api/src/server.js
@@ -13,6 +13,7 @@ const {
   updateUserPassword,
   listPublicNotes,
   listPublicNotesFromUser,
+  deleteUser,
 } = require("logic");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
@@ -97,6 +98,27 @@ connect("mongodb://localhost:27017/demo-db")
         const { sub: id } = payload;
 
         updateUserPassword({ id, currPassword, newPassword })
+          .then(() => res.status(204).send())
+          .catch((error) => res.status(400).json({ error: error.message }));
+      } catch (error) {
+        res.status(400).json({ error: error.message });
+      }
+    });
+
+    api.delete("/users", jsonBodyParser, (req, res) => {
+      try {
+        const {
+          headers: { authorization },
+          body: { password },
+        } = req;
+
+        const [, token] = authorization.split(" ");
+
+        const payload = jwt.verify(token, "mi super secreto");
+
+        const { sub: userId } = payload;
+
+        deleteUser(userId, password)
           .then(() => res.status(204).send())
           .catch((error) => res.status(400).json({ error: error.message }));
       } catch (error) {
--- staff/alba-casas/full-stack/demo-project/logic/src/deleteUser.js
+++ staff/alba-casas/full-stack/demo-project/logic/src/deleteUser.js
@@ -1,9 +1,21 @@
-const { validators: { validateId }} = require("commons");
-const { models: { User }} = require("data");
+const {
+  validators: { validateId, validatePassword },
+} = require("commons");
+const {
+  models: { User },
+} = require("data");
+const { user } = require("data/src/schemas");
 
-function deleteUser(id) {
-  validateId(id)
-  return User.deleteOne({ _id: id });
+function deleteUser(userId, password) {
+  validateId(userId);
+  validatePassword(password);
+
+  return User.findById(userId)
+  .then((user) => {
+    if (user.password === password) {
+      return User.deleteOne({ _id: userId });
+    } else throw new Error("Wrong credentials");
+  });
 }
 
 module.exports = deleteUser;
